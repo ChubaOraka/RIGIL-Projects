820da432bad92e074e9c53972c2b9d2851242de3
hungngovinh <hung.ngovinh97@gmail.com>
2018-06-05 15:33:35 -0400

    Tested TF-IDF and Naive Baiyes

--- scanneroutputandnistclassifications/TF-IDF/Hung/main.py
+++ scanneroutputandnistclassifications/TF-IDF/Hung/main.py
@@ -17,7 +17,7 @@ file = pd.read_csv('sample.csv')
 
 
 #Test data
-x_test = file['description']
+x_final_validate_result = file['description']
 
 #Train_Data
 data = pd.read_csv('data.csv')
@@ -31,13 +31,43 @@ count_vect = CountVectorizer()
 X_train_counts = count_vect.fit_transform(x_train)
 X_train_counts.shape
 
+X_final_validate_result = count_vect.transform(x_final_validate_result)
+X_final_validate_result.shape
+
 #TFIDF Model
 from sklearn.feature_extraction.text import TfidfTransformer
 tfidf_transformer = TfidfTransformer()
 X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 X_train_tfidf.shape
 
+X_final_validate_tfidf = tfidf_transformer.transform(X_final_validate_result)
+X_final_validate_tfidf.shape
+
+#MODIFY Y LABEL DATA
 data['modified description'] = data['name'].str[:2]
 y_train = data['modified description']
+
+#Encoding categorical data ( Y Labels to categories)
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_Y = LabelEncoder()
+y_train[:] = labelencoder_Y.fit_transform(y_train[:])
+
+#Splitting dataset into training set and test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X_train_tfidf, y_train, test_size = 0.2, random_state = 0)
+
+#Naive Bayes Classfier 
+from sklearn.naive_bayes import MultinomialNB
+NBclassifier = MultinomialNB().fit(X_train, Y_train)
+
+#Testing Naive Bayes Classifier
+predicted = NBclassifier.predict(X_test)
+np.mean(predicted == Y_test)
+
+#Validate on Sample.csv file
+sample_predicted = NBclassifier.predict(X_final_validate_tfidf)
+y_final_validate_result = pd.DataFrame(list(labelencoder_Y.inverse_transform(sample_predicted)))
+#result
+result = pd.concat([x_final_validate_result, y_final_validate_result], axis = 1)
 
 
