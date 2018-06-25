599b806a5e0becddf8cbbbb8ca1320fed0023749
Chuba Oraka <17247707+ChubaOraka@users.noreply.github.com>
2018-06-25 18:03:10 -0400

    Tried to add a View widget to the center. Did not work.

--- Android Projects/ParticleEffect/app/src/main/java/com/rigil/particleeffect/MainActivity.java
+++ Android Projects/ParticleEffect/app/src/main/java/com/rigil/particleeffect/MainActivity.java
@@ -34,21 +34,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        ScrollView scroll = findViewById(R.id.scroll);
+//        ScrollView scroll = findViewById(R.id.scroll);
 //        ParticleSystem ps = new ParticleSystem(this, 1000, R.drawable.ic_launcher_background, 100)
 //                .setSpeedRange(0.2f, 0.5f)
 //                .oneShot(scroll, 500);
-        new ParticleSystem(this, 1000, R.drawable.ic_launcher_background, 100)
-                .setSpeedRange(0.2f, 0.5f)
-                .oneShot(scroll, 500);
+//        new ParticleSystem(this, 1000, R.drawable.ic_launcher_background, 100)
+//                .setSpeedRange(0.2f, 0.5f)
+//                .oneShot(scroll, 500);
 
-        new ParticleSystem(this, 80, R.drawable.star_white, 10000)
+
+        new ParticleSystem(this, 80, R.drawable.star_white, 1000)
+                .setSpeedModuleAndAngleRange(0f, 0.3f, 180, 180)
+                .setRotationSpeed(144)
+                .setAcceleration(0.00005f, 90)
+                .emit(findViewById(R.id.helloTextView), 8);
+
+        new ParticleSystem(this, 80, R.drawable.star_white, 1000)
+                .setSpeedModuleAndAngleRange(0f, 0.3f, 0, 360)
+                .setRotationSpeed(144)
+                .setAcceleration(0.00005f, 90)
+                .emit(findViewById(R.id.emiter_center), 8);
+
+
+        new ParticleSystem(this, 80, R.drawable.star_white, 1000)
                 .setSpeedModuleAndAngleRange(0f, 0.3f, 180, 180)
                 .setRotationSpeed(144)
                 .setAcceleration(0.00005f, 90)
                 .emit(findViewById(R.id.emiter_top_right), 8);
 
-        new ParticleSystem(this, 80, R.drawable.star_white, 10000)
+        new ParticleSystem(this, 80, R.drawable.star_white, 1000)
                 .setSpeedModuleAndAngleRange(0f, 0.3f, 0, 0)
                 .setRotationSpeed(144)
                 .setAcceleration(0.00005f, 90)
